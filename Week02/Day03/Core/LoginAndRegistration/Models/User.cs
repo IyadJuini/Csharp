c2ded42b51f5a5faa94bcf16694cccdb1b253781
Iyadh <yad.juini@gmail.com>
2023-10-11 21:24:19 +0200

    Login_And_Registration

---
+++ Week02/Day03/Core/LoginAndRegistration/Models/User.cs
@@ -0,0 +1,48 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // New import for ****
+namespace LoginAndRegistration.Models;
+
+public class User
+{
+    // User Id
+    [Key]
+    public int UserId { get; set; }
+
+    // FirstName
+    [Required(ErrorMessage = "Please enter your username.")]
+    [MinLength(2, ErrorMessage = "Please enter a valid username .")]
+    public string FirstName { get; set; }
+    // LastName
+    [Required(ErrorMessage = "Please enter your username.")]
+    [MinLength(2, ErrorMessage = "Please enter a valid username .")]
+    public string LastName { get; set; }
+
+
+    // Email
+    [Required(ErrorMessage = "Please enter your email.")]
+    [EmailAddress(ErrorMessage = "Please enter a valid email.")]
+    public string Email { get; set; }
+
+    // Password
+    [Required(ErrorMessage = "Please enter your password.")]
+    [DataType(DataType.Password)] // useful
+    [MinLength(8, ErrorMessage = "Please enter a valid Password .")]
+    public string Password { get; set; }
+
+    // Confirm Password
+    [NotMapped]
+    [Required(ErrorMessage = "Please enter your password.")]
+    [Compare("Password", ErrorMessage = "Passwords must match.")]
+    [DataType(DataType.Password)] // useful
+    [Display(Name = "Confirm Password ")]
+    public string ConfirmPassword { get; set; }
+
+
+    // Created At
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    // Updated At
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+}
